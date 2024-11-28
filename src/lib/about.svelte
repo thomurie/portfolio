f78a2403666f9156613d103fe3088f505bfd0a65
Thom Urie <uriethom@gmail.com>
2024-11-28 22:07:12 +1100

    about section

---
+++ src/lib/about.svelte
@@ -0,0 +1,18 @@
+<section>
+	<h2>About</h2>
+	<p>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima voluptates dolore a nam
+		quaerat, animi eaque magnam fuga molestiae repellendus aliquid accusamus nobis, in deleniti
+		asperiores corporis? Nulla, non porro.
+	</p>
+	<p>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima voluptates dolore a nam
+		quaerat, animi eaque magnam fuga molestiae repellendus aliquid accusamus nobis, in deleniti
+		asperiores corporis? Nulla, non porro.
+	</p>
+	<p>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima voluptates dolore a nam
+		quaerat, animi eaque magnam fuga molestiae repellendus aliquid accusamus nobis, in deleniti
+		asperiores corporis? Nulla, non porro.
+	</p>
+</section>
