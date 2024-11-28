c795fff65ac311523c979cf7765617fd18b245cd
Thom Urie <uriethom@gmail.com>
2024-11-28 22:19:01 +1100

    footer section

---
+++ src/lib/footer.svelte
@@ -0,0 +1,10 @@
+<footer>
+	<p>
+		This site was designed in Figma and coded in WebStorm by yours truly. Built with Svelte.js and
+		Tailwind CSS. Deployed with Render
+	</p>
+	<p>
+		If you enjoy my work, or want to have a chat please feel free to connect with me on LinkedIn.
+		Thank you!
+	</p>
+</footer>
