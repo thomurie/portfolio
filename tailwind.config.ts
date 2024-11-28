6392aefe68409deb4d5332d2f57e263e9e233289
Thom Urie <uriethom@gmail.com>
2024-11-28 22:05:31 +1100

    Init setup and config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,15 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'emerald': '#5ECE8D',
+			'blueCrayola': '#377CFB',
+			'safetyOrange': '#F67D65',
+			'charcoal': '#333C4D',
+			'snow': '#FAF7F8',
+		},
 		extend: {}
 	},
 
