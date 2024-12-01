066f4175cb63b7d5f6356ada2267f3cdc02e6e83
Thom Urie <uriethom@gmail.com>
2024-12-01 18:11:14 +1100

    create link component

---
+++ src/lib/components/ui/Link.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { type ILink } from '$lib/types';
+	let { href, target, rel, ariaLabel = '', label }: ILink = $props();
+</script>
+
+<a
+	{href}
+	{target}
+	{rel}
+	aria-label={ariaLabel}
+	class="block leading-none underline underline-offset-2 transition-colors duration-200 ease-in-out hover:text-blueCrayola focus:text-blueCrayola dark:hover:text-safetyOrange dark:focus:text-safetyOrange"
+>
+	{label}
+</a>
