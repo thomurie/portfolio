458612719889366cf79904e7bd0e632622ef2cdc
Thom Urie <uriethom@gmail.com>
2024-12-01 16:50:56 +1100

    create header component

---
+++ src/lib/header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { mainLinks } from '$lib/data/mainLinks';
+</script>
+
+<header class="space-y-1">
+	<h1 class="text-2xl font-bold">Thomas Urie</h1>
+	<h2 class="font-semibold">Full Stack Engineer</h2>
+	<p>I build high performing, data rich applications for the web.</p>
+	<ul class="flex flex-row items-center justify-start gap-4">
+		{#each mainLinks as link}
+			<li>
+				<a
+					href={link.url}
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label={link.label}
+					class="block leading-none underline underline-offset-2 transition-colors duration-200 ease-in-out hover:text-blueCrayola focus:text-blueCrayola"
+				>
+					{link.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,33 +2,23 @@
 	import About from '$lib/about.svelte';
 	import Experience from '$lib/experience.svelte';
 	import Footer from '$lib/footer.svelte';
-	import Projects from '$lib/projects.svelte';
+	import Header from '$lib/header.svelte';
 </script>
 
 <svelte:head>
 	<title>Thomas Urie</title>
+	<meta name="description" content="Thomas Urie's Portfolio" />
 	<meta property="og:title" content="Thomas Urie" />
 	<meta property="og:description" content="Thomas Urie's Portfolio" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="Thomas Urie" />
 	<!-- <meta property="og:url" content="https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg" /> -->
-	<!-- <meta property="og:image" content="https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg" /> -->
 </svelte:head>
-<div class="mx-auto my-10 max-w-[650px]">
-	<header class="space-y-1">
-		<h1 class="text-2xl font-bold">Thomas Urie</h1>
-		<h2 class="font-semibold">Full Stack Engineer</h2>
-		<p>I build high performing, data rich applications for the web.</p>
-		<ul>
-			<li><a href="https://localhost:5173/">Github</a></li>
-			<li><a href="https://localhost:5173/">LinkedIn</a></li>
-			<li><a href="https://localhost:5173/">Bluesky</a></li>
-		</ul>
-	</header>
+<div class="mx-auto my-10 max-w-[650px] px-4 md:px-0">
+	<Header />
 	<main class="mt-8 space-y-8">
 		<About />
 		<Experience />
-		<Projects />
 		<Footer />
 	</main>
 </div>
