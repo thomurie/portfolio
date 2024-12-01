d1e17eca5fcbee7aa2bfdc50b19362af6ded62c6
Thom Urie <uriethom@gmail.com>
2024-12-01 18:11:29 +1100

    update error page UI

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import { Link } from '$lib/components/ui/Link.svelte';
+</script>
+
 <svelte:head>
 	<title>404</title>
 	<meta name="description" content="Thomas Urie's Portfolio" />
@@ -7,9 +11,9 @@
 	<meta property="og:site_name" content="Thomas Urie" />
 	<!-- <meta property="og:url" content="https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg" /> -->
 </svelte:head>
-<main class="m-auto space-y-4">
+<main class="mx-auto my-10 max-w-[650px] px-4 md:px-0">
 	<h1 class="text-2xl font-bold">404</h1>
-	<p>Page not found</p>
+	<h2 class="text-xl font-bold">Page not found</h2>
 	<p>Sorry, we couldn't find the page you were looking for.</p>
-	<a href="/">Go back to home</a>
+	<Link href="/" label="Go back to home" />
 </main>
